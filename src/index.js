7d7db256db140fe4beaa3afe9ba282c8fcc0ecf0
Dzmitry Zakhar <31858665+SpiritForest@users.noreply.github.com>
2018-02-22 23:50:01 +0300

    1
    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    var arrLength, check, counterOpenBracket = 0, counterCloseBracket = 0;
+    var searchSub = '', summ = 0;
+    arrLength = bracketsConfig.length;
+
+//check of quantity of open and close brackets
+    for (i = 0; i < arrLength; i++) {
+        for (k = 0; k < str.length; k++) {
+            if (bracketsConfig[i][0] == str.charAt(k)) counterOpenBracket++;
+            if (bracketsConfig[i][1] == str.charAt(k)) counterCloseBracket++;
+        }
+    }
+    check = (counterCloseBracket == counterOpenBracket);
+
+    for (i = 0; i < arrLength; i++) {
+        for (k = 0; k < str.length; k++) {
+            if (bracketsConfig[i][0] == str.charAt(k)) summ++;
+            if (bracketsConfig[i][1] == str.charAt(k)) summ--;
+            if (summ < 0) return false;
+        }
+    }
+
+    check = (counterCloseBracket == counterOpenBracket);
+
+    //closing opening case like this "[)"
+   if (arrLength > 1) {
+        outer:
+    for (i = 0; i < arrLength; i++) {
+        for (k = 1+i; k < arrLength; k++) {
+            if (bracketsConfig[i][0] == "|" || bracketsConfig[k][1] == "|") continue;
+            searchSub = bracketsConfig[i][0] + bracketsConfig[k][1];
+            if (str.indexOf(searchSub) > -1) {
+                check = false;
+                break outer;
+            }
+            }
+        }
+    }
+
+
+    return check;
 }
