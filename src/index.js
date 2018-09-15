be4d5bc5a27e07fe8f9b9f0b1bfe43fb675b1311
SpiritForest <dzahar@mail.ru>
2018-09-15 21:13:36 +0300

    completed brackets

--- src/index.js
+++ src/index.js
@@ -1,43 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
+  var stack = [];
+  var strArr = str.split("");
+  stack[0] = strArr[0];
+  var nxtBracket = 1;
 
-    var arrLength, check, counterOpenBracket = 0, counterCloseBracket = 0;
-    var searchSub = '', summ = 0;
-    arrLength = bracketsConfig.length;
-
-//check of quantity of open and close brackets
-    for (i = 0; i < arrLength; i++) {
-        for (k = 0; k < str.length; k++) {
-            if (bracketsConfig[i][0] == str.charAt(k)) counterOpenBracket++;
-            if (bracketsConfig[i][1] == str.charAt(k)) counterCloseBracket++;
-        }
-    }
-    check = (counterCloseBracket == counterOpenBracket);
-
-    for (i = 0; i < arrLength; i++) {
-        for (k = 0; k < str.length; k++) {
-            if (bracketsConfig[i][0] == str.charAt(k)) summ++;
-            if (bracketsConfig[i][1] == str.charAt(k)) summ--;
-            if (summ < 0) return false;
-        }
-    }
-
-    check = (counterCloseBracket == counterOpenBracket);
-
-    //closing opening case like this "[)"
-   if (arrLength > 1) {
-        outer:
-    for (i = 0; i < arrLength; i++) {
-        for (k = 1+i; k < arrLength; k++) {
-            if (bracketsConfig[i][0] == "|" || bracketsConfig[k][1] == "|") continue;
-            searchSub = bracketsConfig[i][0] + bracketsConfig[k][1];
-            if (str.indexOf(searchSub) > -1) {
-                check = false;
-                break outer;
-            }
-            }
-        }
-    }
-
-
-    return check;
-}
+  while(nxtBracket < strArr.length){
+  stack.push(strArr[nxtBracket]);
+  for (var i = 0; i < bracketsConfig.length; i++){
+   if (stack[stack.length-2] == bracketsConfig[i][0] && stack[stack.length-1] == bracketsConfig[i][1]){
+    stack.pop();
+    stack.pop();
+   } // end if
+  } // end for
+    nxtBracket++;
+  } // end while
+  return stack.length ? false : true;
+} //end function
